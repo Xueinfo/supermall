c34fa2c22c7b739e31f8cf77c74e3e2f728a11d4
Xueinfo <wy972511184@163.com>
2020-04-12 11:02:17 +0800

    css文件，配置完毕

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+export default{
+    configuerwebpack:{
+        resolve:{
+            alias:{
+                "assest":"src/assets",
+                "common":"src/common",
+                "components":"src/components",
+                "network":"src/network",
+                "views":"src/views"
+            }
+        }
+    }
+}
