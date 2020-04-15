c6a80c4500c77d40852c2f130afb423a6b240dc8
Xueinfo <wy972511184@163.com>
2020-04-15 18:32:04 +0800

    请求首页数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -9,10 +9,25 @@
 </template>
 <script>
 import navbar from "components/common/navbar/NavBar"
+import {getHomeMultidata} from "network/home"
+
 export default {
     name:"home",
     components:{
         navbar
+    },
+    data(){
+        return{
+            banners:[],
+            recommends:[]
+        }
+    },
+    created(){
+        getHomeMultidata().then(res=>{
+            console.log(res)
+            this.banners=res.data.banners;
+            this.recommends=res.data.recommends;
+        })
     }
 }
 </script>
